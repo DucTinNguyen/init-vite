840e0db8516bf1716d9b63cd16261f219b9caf03
tin nguyen <tinndev21@gmail.com>
2024-05-07 13:43:54 +0700

    init source

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='text-2xl text-red-500'>Hello</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/roadmap/index.tsx
@@ -0,0 +1,5 @@
+const Roamap = () => {
+  return <div>Roamap</div>
+}
+
+export default Roamap
